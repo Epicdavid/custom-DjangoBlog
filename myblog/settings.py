b3d16aab72cf1b0100df6688f1369349a1e49ef7
Epic david <toonerdrill@gmail.com>
2020-01-28 05:36:43 +0100

    deploying

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,4 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 
 LOGIN_URL = 'templates/registration/login'
 LOGIN_REDIRECT_URL = 'templates/blog/post-list.html'
+django_heroku.settings(locals())
